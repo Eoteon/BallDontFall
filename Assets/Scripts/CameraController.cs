b47836b3813065cb7dc60a1055c96138f922e9a1
Eoteon <Eoteon@gmail.com>
2019-05-16 23:15:01 -0400

    Cleanup movement and camera rotation scripts and created sample golf course

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,56 +5,35 @@ using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
-	public float minFov = 15f;
-	public float maxFov = 150f;
-	public float fovSensitivity = 10f;
-	public GameObject player;
+	public const float Y_ANGLE_MIN = 10f;
+	public const float Y_ANGLE_MAX = 50f;
 
-	private Vector3 offset;
-	private Vector2 rotation;
-	private int fovZoomCount = 0;
+	public Transform target;
+	public Vector3 offset;
+	public bool useOffsetVals;
+	public float rotationSpeed;
+	public Transform pivot;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-		offset = transform.position - player.transform.position;
-		rotation = new Vector2(0, 0);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-		updateFov();
-		transform.position = player.transform.position + offset;
-		updateVerticalRotation();
-		//rotation.y += Input.GetAxis("Mouse X");
-		//rotation.x -= Input.GetAxis("Mouse Y");
-		//transform.eulerAngles = (Vector2)rotation * 3;
-		updateDebugText();
-    }
-
-	void updateVerticalRotation() {
-		float mouseYMovement = Input.GetAxis("Mouse Y") / 20f;
-		Quaternion newRotation = player.transform.rotation;
-		newRotation.x = transform.rotation.x - mouseYMovement;
-		newRotation.z = transform.rotation.z;
-		transform.rotation = newRotation;
-	}
-
-	void updateFov() {
-		float newFov = Camera.main.fieldOfView + Input.GetAxis("Mouse ScrollWheel") * fovSensitivity * -1;
-		if (player.GetComponent<PlayerController>().isBoosting && fovZoomCount < 100) {
-			newFov += 0.1f;
-			fovZoomCount++;
-		} else if (fovZoomCount > 0) {
-			newFov -= 0.1f;
-			fovZoomCount--;
+	void Start() {
+		if (!useOffsetVals) {
+			offset = target.position - transform.position;
 		}
-		Camera.main.fieldOfView = Mathf.Clamp(newFov, minFov, maxFov);
+
+		pivot.transform.position = target.transform.position;
+		pivot.transform.parent = target.transform;
+
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 
-	void updateDebugText() {
-		Text cameraInfoText = GameObject.FindGameObjectWithTag("CameraInfoText").GetComponent<Text>();
-		cameraInfoText.text = "Rotation(x,y,z): (" + transform.rotation.x + "," + transform.rotation.y + "," + transform.rotation.z + ")";
+	void Update() {
+		target.Rotate(Input.GetAxis("Mouse Y") * -rotationSpeed, Input.GetAxis("Mouse X") * rotationSpeed, 0);
+	
+
+		float desiredYAngle = target.eulerAngles.y;
+		float desiredXAngle = target.eulerAngles.x;
+		Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+		transform.position = target.position - (rotation * offset);
+
+		transform.LookAt(target);
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,108 +4,37 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
+	
+	private CharacterController controller;
 
-	private Renderer renderer;
-	private Rigidbody player;
-	private List<GameObject> platformList;
-	private int itemCount = -1;
-	private float lastBoostTime;
+	public float moveSpeed = 100f;
+	public float jumpHeight = 10f;
 
-	public float moveSpeed = 10f;
-	public bool isBoosting;
-	public GameObject bulletPrefab;
+	private Vector3 moveDirection;
 
 	void Start() {
-		renderer = GetComponent<Renderer>();
-		player = GetComponent<Rigidbody>();
-		player.freezeRotation = true;
-		platformList = new List<GameObject>();
-		lastBoostTime = Time.time;
-		incrementItemCount();
+		controller = GetComponent<CharacterController>();
 	}
 
 	void Update() {
-		updateCanvasText();
-	}
+		moveDirection = ((transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"))) * moveSpeed;
+		moveDirection = moveDirection.normalized;
 
-	void LateUpdate() {
-		movePlayer(0f);
-		checkForFloor();
-		handleShoot();
-	}
-
-	void movePlayer(float axisYMovement) {
-		float axisXMovement = Input.GetAxis("Horizontal");
-		float axisZMovement = Input.GetAxis("Vertical");
-
-		if (Input.GetKey("space")) {
-			axisYMovement += 1f;
-			isBoosting = true;
-		} else if (Time.time - lastBoostTime > 1){
-			isBoosting = false;
-			lastBoostTime = Time.time;
-		}
-		
-		Vector3 movement = new Vector3(axisXMovement, axisYMovement, axisZMovement);
-		player.AddForce(movement * moveSpeed, ForceMode.Acceleration);
-		rotatePlayer();
-	}
-
-	void rotatePlayer() {
-		float mouseXMovement = Input.GetAxis("Mouse X") / 20f;
-		if (mouseXMovement != 0) {
-			player.transform.rotation = new Quaternion(player.transform.rotation.x, player.transform.rotation.y + mouseXMovement, player.transform.rotation.z, player.transform.rotation.w);
-		}
-	}
-
-	void checkForFloor() {
-		if (player.transform.position.y <0.5f) {
-			player.transform.position = new Vector3(player.transform.position.x, 0.5f, player.transform.position.z);
-			
-			if (platformList.Count == 3) {
-				Destroy(platformList[0]);
-				platformList.RemoveAt(0);
+		if (controller.isGrounded) {
+			moveDirection.y = 0f;
+			if (Input.GetButtonDown("Jump")) {
+				moveDirection.y = jumpHeight;
 			}
-
-			GameObject newPlatform = GameObject.CreatePrimitive(PrimitiveType.Cube);
-			newPlatform.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - 1, player.transform.position.z);
-			platformList.Add(newPlatform);
 		}
-	}
 
-	void handleShoot() {
-		if (Input.GetMouseButton(0)) {
-			Instantiate(bulletPrefab, player.transform.position, player.transform.rotation);
-		}
-	}
-
-	void OnTriggerEnter(Collider otherObject) {
-		if (otherObject.gameObject.CompareTag("Item")) {
-			handleItemInteraction(otherObject);
-		} else if (otherObject.gameObject.CompareTag("JumpPlatform")) {
-			handleJumpPlatform();
-		}
-	}
-
-	void handleItemInteraction(Collider itemObject) {
-		itemObject.gameObject.SetActive(false);
-		ItemBehavior itemBehavior = itemObject.GetComponent<ItemBehavior>();
-		itemBehavior.startRespawn();
-		incrementItemCount();
-	}
-
-	void incrementItemCount() {
-		itemCount++;
-	}
-
-	void handleJumpPlatform() {
-		movePlayer(200f);
+		moveDirection.y += Physics.gravity.y * 1.5f;
+		controller.Move(moveDirection * Time.deltaTime);
+		updateCanvasText();
 	}
 
 	void updateCanvasText() {
 		Text ballInfoText = GameObject.FindWithTag("BallInfoText").GetComponent<Text>();
-		ballInfoText.text = "Velocity(x,y): (" + player.velocity.x + "," + player.velocity.y + ")"
-			+ "\nPos(x,y,z): (" + player.transform.position.x + "," + player.transform.position.y + "," + player.transform.position.z + ")";
+		ballInfoText.text = "\nPos(x,y,z): (" +  transform.position.x + "," +  transform.position.y + "," +  transform.position.z + ")";
 	}
 
 }
